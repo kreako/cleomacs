0aa9cc1ea35bd16204702c58aff801ea08a31f17
kreako <olivier@kreako.fr>
2021-12-24 21:14:54 +0100

    do not launch if already up

--- postgres.sh
+++ postgres.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-podman run -d \
-  --restart=always \
-  -e POSTGRES_PASSWORD="password" \
-  -v ./pgdata/:/var/lib/postgresql/data:z \
-  --name cleomacs-db \
-  -p 5432:5432 \
-  postgres
+podman ps | grep cleomacs-db > /dev/null
+
+if [ $? -eq 1 ]; then
+  podman run -d \
+    --restart=always \
+    -e POSTGRES_PASSWORD="password" \
+    -v ./pgdata/:/var/lib/postgresql/data:z \
+    --name cleomacs-db \
+    -p 5432:5432 \
+    postgres
+  echo "postgres launched !"
+else
+  echo "postgres already running"
+fi
