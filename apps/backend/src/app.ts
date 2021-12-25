00da148a632b06fc0e6a90c55dd1afb1606e1b1b
kreako <olivier@kreako.fr>
2021-12-26 00:26:17 +0100

    Add custom error handler

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,6 +1,7 @@
 import { PrismaClient, Prisma } from ".prisma/client"
 import express from "express"
 import morgan from "morgan"
+import "express-async-errors"
 
 const prisma = new PrismaClient()
 export const app = express()
@@ -10,5 +11,19 @@ app.use(express.json())
 app.use(morgan("dev"))
 
 app.get("/meuh", async (req, res) => {
-  res.json({ meuh: true })
+  // res.json({ meuh: true })
+  throw new Error("Oups !")
 })
+
+app.use(
+  (error: Error, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
+    console.error("Error : ", error)
+    if (process.env.NODE_ENV === "development") {
+      res
+        .status(500)
+        .json({ error: true, message: error.message, name: error.name, stack: error.stack })
+    } else {
+      res.status(500).json({ error: true })
+    }
+  }
+)
