f39372c26bec475d935e0461e11ae446480fce68
kreako <olivier@kreako.fr>
2021-12-25 22:30:59 +0100

    morgan middleware for logging

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,11 +1,13 @@
 import { PrismaClient, Prisma } from ".prisma/client"
 import express from "express"
+import morgan from "morgan"
 
 const prisma = new PrismaClient()
 export const app = express()
 
 app.disable("x-powered-by")
 app.use(express.json())
+app.use(morgan("dev"))
 
 app.get("/meuh", async (req, res) => {
   res.json({ meuh: true })
