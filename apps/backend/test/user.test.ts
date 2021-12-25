25f72b4cc9b124f74e101623ac6302b08404d7e7
kreako <olivier@kreako.fr>
2021-12-25 19:07:04 +0100

    Add a test for the /meuh route

--- apps/backend/test/user.test.ts
+++ apps/backend/test/user.test.ts
@@ -1,3 +1,12 @@
+import { app } from "../src/app"
+import supertest from "supertest"
+
 test("first test", () => {
   expect(1).toBe(1)
+})
+
+test("meuh test", async () => {
+  const server = supertest(app)
+  const res = await server.get("/meuh").expect(200)
+  expect(res.body.meuh).toBeTruthy()
 })
