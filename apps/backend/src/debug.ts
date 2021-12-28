c2a4f4ea875f41de68cbec71bcb16dbb9a3ceef8
kreako <olivier@kreako.fr>
2021-12-28 17:37:27 +0100

    Add headers in debug's log

--- apps/backend/src/debug.ts
+++ apps/backend/src/debug.ts
@@ -2,6 +2,6 @@ import express from "express"
 
 export const debugLogger =
   (tag: string) => (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log("log", tag, req.url, req.body)
+    console.log("log", tag, req.url, req.body, req.headers)
     next()
   }
