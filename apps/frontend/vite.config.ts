7da49f869ea001d9c00204c2791ebb7f0d46822e
kreako <olivier@kreako.fr>
2022-01-09 01:02:14 +0100

    Add unplugin-icons dependencies

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import Icons from "unplugin-icons/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Icons({ compiler: "jsx", jsx: "react" })],
   server: {
     host: "0.0.0.0",
     proxy: {
