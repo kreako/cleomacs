3650ebaf969d683cd6b7489042c48031ab16cd96
kreako <olivier@kreako.fr>
2022-01-12 01:31:18 +0100

    useProfile wrapper api

--- apps/frontend/src/api/auth.ts
+++ apps/frontend/src/api/auth.ts
@@ -1,9 +1,20 @@
 import axios from "axios"
-import type { SignupInputType } from "@cleomacs/api/auth"
+import type { ProfileOutputType, SignupInputType } from "@cleomacs/api/auth"
+import { useQuery } from "react-query"
+import { keys } from "./query-key"
 
 export const signup = async (values: SignupInputType) => {
   await axios.post("/api/auth/signup", values, {
     timeout: 3000,
     transitional: { clarifyTimeoutError: true },
   })
+}
+
+const fetchProfile = async (): Promise<ProfileOutputType> => {
+  const response = await axios.get("/api/auth/profile")
+  return response.data
+}
+
+export const useProfile = () => {
+  return useQuery(keys.profile, fetchProfile)
 }
