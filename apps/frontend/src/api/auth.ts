80c6d89adc73085ed0a5b84aa5d2fdba375a3ccf
kreako <olivier@kreako.fr>
2022-01-12 15:41:54 +0100

    Add useLogout and a default axios config

--- apps/frontend/src/api/auth.ts
+++ apps/frontend/src/api/auth.ts
@@ -1,17 +1,41 @@
-import axios from "axios"
+import rawAxios from "axios"
 import type { ProfileOutputType, SignupInputType } from "@cleomacs/api/auth"
-import { useQuery } from "react-query"
+import { useMutation, useQuery } from "react-query"
 import { keys } from "./query-key"
 
+const axios = rawAxios.create({
+  baseURL: "/api",
+  timeout: 3000,
+  transitional: { clarifyTimeoutError: true },
+})
+
 export const postSignup = async (values: SignupInputType) => {
-  await axios.post("/api/auth/signup", values, {
-    timeout: 3000,
-    transitional: { clarifyTimeoutError: true },
-  })
+  return await axios.post("/auth/signup", values)
+}
+
+export const postLogout = async () => {
+  return await axios.post("/auth/logout", {})
+}
+
+type UseLogoutType = {
+  onError: (error: Error) => void
+  onSuccess: () => void
+}
+
+export const useLogout = ({ onError, onSuccess }: UseLogoutType) => {
+  return useMutation(
+    async () => {
+      return await postLogout()
+    },
+    {
+      onError,
+      onSuccess,
+    }
+  )
 }
 
 const fetchProfile = async (): Promise<ProfileOutputType> => {
-  const response = await axios.get("/api/auth/profile")
+  const response = await axios.get("/auth/profile")
   return response.data
 }
 
