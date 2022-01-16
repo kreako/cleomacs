ecbf03c1d5d15930aba30cd211a923f1d42fbfbd
kreako <olivier@kreako.fr>
2022-01-16 21:05:48 +0100

    ErrorBoundary on MainLayout with default content - to be customized

--- apps/frontend/src/layout/MainLayout.tsx
+++ apps/frontend/src/layout/MainLayout.tsx
@@ -1,9 +1,16 @@
+import { ErrorBoundary } from "react-error-boundary"
 import { Outlet } from "react-router"
+import MainErrorFallback from "../components/MainErrorFallback"
 
-export default function EmptyLayout() {
+export default function MainLayout() {
   return (
-    <>
+    <ErrorBoundary
+      FallbackComponent={MainErrorFallback}
+      onReset={() => {
+        // TODO ?
+      }}
+    >
       <Outlet />
-    </>
+    </ErrorBoundary>
   )
 }
