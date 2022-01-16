1d188dfccdaf46c2942b790922fbe4ec49a98a4a
kreako <olivier@kreako.fr>
2022-01-16 21:04:54 +0100

    Add an AuthenticationError and throw it from query hook to be catched by an ErrorBoundary

--- apps/frontend/src/api/auth.ts
+++ apps/frontend/src/api/auth.ts
@@ -1,4 +1,4 @@
-import rawAxios from "axios"
+import rawAxios, { AxiosError } from "axios"
 import type {
   LoginInputType,
   ProfileOutputType,
@@ -12,6 +12,14 @@ const axios = rawAxios.create({
   timeout: 3000,
   transitional: { clarifyTimeoutError: true },
 })
+
+export class AuthenticationError extends Error {
+  name = "AuthenticationError"
+  statusCode = 401
+  constructor(message = "Vous devez vous connecter pour accéder à cette page") {
+    super(message)
+  }
+}
 
 export const postSignup = async (values: SignupInputType) => {
   return await axios.post("/auth/signup", values)
@@ -77,10 +85,30 @@ export const useLogout = ({ onError, onSuccess }: UseLogoutType) => {
 }
 
 const fetchProfile = async (): Promise<ProfileOutputType> => {
-  const response = await axios.get("/auth/profile")
-  return response.data
+  try {
+    const response = await axios.get("/auth/profile")
+    return response.data
+  } catch (error) {
+    if (rawAxios.isAxiosError(error)) {
+      const axiosError = error as AxiosError
+      if (axiosError.response?.status === 401) {
+        throw new AuthenticationError()
+      }
+    }
+    throw error
+  }
+}
+
+export const retryQuery = (count: number, error: unknown): boolean => {
+  if (error instanceof AuthenticationError) {
+    return false
+  }
+  return count < 3
 }
 
 export const useProfile = () => {
-  return useQuery(keys.profile, fetchProfile)
+  return useQuery(keys.profile, fetchProfile, {
+    useErrorBoundary: true,
+    retry: retryQuery,
+  })
 }
