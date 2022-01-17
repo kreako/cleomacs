a0efb4798fd19c4d306750ae16a05ccbfe08a46f
kreako <olivier@kreako.fr>
2022-01-17 10:26:12 +0100

    Add hookTimeout to vitest config

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   test: {
     setupFiles: ["test/setup.ts"],
-    testTimeout: 60 * 60 * 1000,
+    testTimeout: 60000,
+    hookTimeout: 60000,
   },
 })
