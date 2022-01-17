a3f7f9900d255a9a18ce94c6c3a3a258cbf060a2
kreako <olivier@kreako.fr>
2022-01-17 12:04:19 +0100

    Main ErrorBoundary now displays the LoginForm on 401 error

--- apps/frontend/src/components/MainErrorFallback.tsx
+++ apps/frontend/src/components/MainErrorFallback.tsx
@@ -1,14 +1,55 @@
 import { FallbackProps } from "react-error-boundary"
+import { AuthenticationError, useLogin } from "../api/auth"
+import RawError from "./RawError"
+import type { LoginInputType } from "@cleomacs/api/auth"
+import LoginForm from "../components/LoginForm"
+
+type LoginProps = {
+  onSuccess: () => void
+}
+
+function LoginOn401(props: LoginProps) {
+  const login = useLogin({
+    onError: (error) => {
+      // TODO
+      console.log("onError", JSON.stringify(error))
+    },
+    onSuccess: () => {
+      props.onSuccess()
+    },
+  })
+  const onSubmit = async (values: LoginInputType) => {
+    await login.mutate(values)
+  }
+  return (
+    <LoginForm
+      onSubmit={onSubmit}
+      mainError={login.error as Error}
+      loading={login.isLoading}
+    />
+  )
+}
 
 export default function MainErrorFallback({
   error,
   resetErrorBoundary,
 }: FallbackProps) {
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
-  )
+  if (error instanceof AuthenticationError) {
+    return (
+      <div className="pt-4 mx-2 flex flex-col items-center">
+        <div className="text-indigo-600 font-bold">
+          Connectez-vous pour accéder à cette page !
+        </div>
+        <LoginOn401 onSuccess={resetErrorBoundary} />
+      </div>
+    )
+  } else {
+    // default ugly one
+    return (
+      <div role="alert">
+        <RawError error={error} />
+        <button onClick={resetErrorBoundary}>Try again</button>
+      </div>
+    )
+  }
 }
