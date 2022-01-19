b223cc4c0ec8aa4e8b13139cda733d81a6c70a5a
kreako <olivier@kreako.fr>
2022-01-19 20:54:27 +0100

    Wire preview-email to the mailer

--- apps/backend/src/mailer.ts
+++ apps/backend/src/mailer.ts
@@ -1,3 +1,20 @@
+import previewEmail from "preview-email"
+
 export const lostPasswordMail = async (to: string, token: string) => {
-  console.log("lostPasswordMail", to, token)
+  // TODO from, subject, html, text... TODO
+  const message = {
+    from: "todo@todo",
+    to,
+    subject: "[TODO]Mot de passe perdu",
+    html: `<p><a href="http://127.0.0.1:3001/#/change-lost-password?token=${token}">Changer votre mot de passe ici : </a></p>`,
+    text: `
+    Changer votre mot de passe ici :',
+    http://127.0.0.1:3001/#/change-lost-password?token=${token}
+    `,
+  }
+  if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
+    await previewEmail(message)
+  } else {
+    throw new Error(`TODO lostPasswordMail ${to} ${token}`)
+  }
 }
