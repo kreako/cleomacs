2471fa89a1b8fe77403e7a514cb914851831c529
kreako <olivier@kreako.fr>
2022-01-19 13:12:56 +0100

    Add lost-password/change api input/output type

--- packages/api/auth.ts
+++ packages/api/auth.ts
@@ -35,3 +35,20 @@ export const profileOutput = (user: User) => ({
   user,
 })
 export type ProfileOutputType = ReturnType<typeof profileOutput>
+
+// lost password
+export const lostPasswordInput = z.object({
+  email,
+})
+export type LostPasswordInput = z.infer<typeof lostPasswordInput>
+export const lostPasswordOutput = () => success
+export type LostPasswordOutput = ReturnType<typeof lostPasswordOutput>
+
+// change lost password
+export const changeLostPasswordInput = z.object({
+  token: z.string(),
+  password,
+})
+export type ChangeLostPasswordInput = z.infer<typeof changeLostPasswordInput>
+export const changeLostPasswordOutput = (success: boolean) => ({ success })
+export type ChangeLostPasswordOutput = ReturnType<typeof changeLostPasswordOutput>
