644592752772bf5f78aa84a3e0c1c9a050c97fbe
kreako <olivier@kreako.fr>
2022-01-22 21:36:17 +0100

    Add a post utility similar to the get one

--- apps/frontend/src/api/utils.ts
+++ apps/frontend/src/api/utils.ts
@@ -33,6 +33,21 @@ export const rawGet = async <T>(url: string): Promise<AxiosResponse<T>> => {
   return await axios.get(url)
 }
 
+export const post = async <T, D>(url: string, data: D): Promise<T> => {
+  try {
+    const response = await axios.post(url, data)
+    return response.data
+  } catch (error) {
+    if (rawAxios.isAxiosError(error)) {
+      const axiosError = error as AxiosError
+      if (axiosError.response?.status === 401) {
+        throw new AuthenticationError()
+      }
+    }
+    throw error
+  }
+}
+
 export const rawPost = async <T, D>(
   url: string,
   data: D
