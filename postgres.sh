b339f47c9eb40f88a45600b2446d8acab87ec92d
kreako <olivier@kreako.fr>
2022-01-22 13:15:19 +0100

    postgres launch script - Handle the case where container is stopped

--- postgres.sh
+++ postgres.sh
@@ -3,14 +3,23 @@
 podman ps | grep cleomacs-db > /dev/null
 
 if [ $? -eq 1 ]; then
-  podman run -d \
-    --restart=always \
-    -e POSTGRES_PASSWORD="password" \
-    -v ./pgdata/:/var/lib/postgresql/data:z \
-    --name cleomacs-db \
-    -p 5432:5432 \
-    postgres
-  echo "postgres launched !"
+  # postgres not running
+  # Maybe this need to be restarted
+  podman ps -a | grep cleomacs-db > /dev/null
+  if [ $? -eq 1 ]; then
+    # No, need to run
+    podman run -d \
+      --restart=always \
+      -e POSTGRES_PASSWORD="password" \
+      -v ./pgdata/:/var/lib/postgresql/data:z \
+      --name cleomacs-db \
+      -p 5432:5432 \
+      postgres
+    echo "postgres launched !"
+  else
+    podman start cleomacs-db
+    echo "postgres started !"
+  fi
 else
   echo "postgres already running"
 fi
