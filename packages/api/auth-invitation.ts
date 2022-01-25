26004dadc3af586699bce83be1f8fc68cc1801c1
kreako <olivier@kreako.fr>
2022-01-25 23:46:52 +0100

    Add token info input/output type for invitation link

--- packages/api/auth-invitation.ts
+++ packages/api/auth-invitation.ts
@@ -9,3 +9,27 @@ export const newInvitationInput = z.object({
 export type NewInvitationInput = z.infer<typeof newInvitationInput>
 export const newInvitationOutput = (token: string) => ({ token })
 export type NewInvitationOutput = ReturnType<typeof newInvitationOutput>
+
+// token info
+export const tokenInfoInput = z.object({
+  token: z.string(),
+})
+export type TokenInfoInput = z.infer<typeof tokenInfoInput>
+export const tokenInfoOutput = (info: TokenInfoOutput) => info
+export type TokenInfo = {
+  inviterName: string | null
+  inviterEmail: string
+  invitedUserId?: number
+  invitedName: string | null
+  invitedEmail: string
+  createdAt: Date
+  organizationName: string
+}
+export type TokenInfoOutput =
+  | {
+      success: false
+    }
+  | {
+      success: true
+      token: TokenInfo
+    }
