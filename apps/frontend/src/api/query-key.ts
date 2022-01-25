9a2f7f12e7fd2f2efc6f3d9a40077a04913ff710
kreako <olivier@kreako.fr>
2022-01-25 12:11:19 +0100

    query key to be type as const

--- apps/frontend/src/api/query-key.ts
+++ apps/frontend/src/api/query-key.ts
@@ -1,6 +1,5 @@
 export const keys = {
   profile: [{ scope: "profile" }] as const,
-  authPasswordTokenInfo: (token: string | null) => [
-    { scope: "auth-password", token },
-  ],
+  authPasswordTokenInfo: (token: string | null) =>
+    [{ scope: "auth-password", token }] as const,
 }
