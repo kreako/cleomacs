3bad1ea5ff440f7875992d3da9832317ece6bd2d
kreako <olivier@kreako.fr>
2022-02-01 18:58:35 +0100

    Use NameForm in SettingsAccount page with profile query

--- apps/frontend/src/pages/SettingsAccount.tsx
+++ apps/frontend/src/pages/SettingsAccount.tsx
@@ -3,6 +3,10 @@ import LabelInput from "../components/LabelInput"
 import Loading from "../components/Loading"
 import Logout from "../components/Logout"
 import { required } from "../utils/form"
+import { useProfile } from "../api/auth"
+import RawError from "../components/RawError"
+import ErrorCard from "../components/ErrorCard"
+
 type NameFormProps = {
   loading: boolean
   initialName: string | undefined
@@ -73,6 +77,45 @@ export function NameForm({ loading, initialName, onSubmit }: NameFormProps) {
     </div>
   )
 }
+
 export default function SettingsAccount() {
-  return <div className="pl-4">Account settings !</div>
+  const onNameSubmit = async (name: string) => {
+    console.log("onNameSubmit", name)
+  }
+
+  const profile = useProfile()
+  if (profile.data) {
+    if (profile.data.user) {
+      return (
+        <div className="pl-4 mt-4">
+          <div>
+            <div className="text-sky-900">Votre email:</div>
+            <div>{profile.data.user.email}</div>
+          </div>
+          <div className="mt-4">
+            <NameForm
+              loading={false}
+              onSubmit={onNameSubmit}
+              initialName={profile.data.user.name || undefined}
+            />
+          </div>
+          <div className="mt-4">
+            <Logout />
+          </div>
+        </div>
+      )
+    } else {
+      // TODO error reporting
+      return <ErrorCard>Unknown state: {JSON.stringify(profile)}</ErrorCard>
+    }
+  }
+  if (profile.isLoading) {
+    return <Loading size={2} />
+  }
+  if (profile.isError) {
+    // TODO error reporting
+    return <RawError error={profile.error as Error} />
+  }
+  // TODO error reporting
+  return <ErrorCard>Unknown state: {JSON.stringify(profile)}</ErrorCard>
 }
