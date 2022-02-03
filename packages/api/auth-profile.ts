cc9a4ca692a61141b0ca6521ac10b041720e8d35
kreako <olivier@kreako.fr>
2022-02-03 15:33:25 +0100

    Add update-user-name input/output type in api package

--- packages/api/auth-profile.ts
+++ packages/api/auth-profile.ts
@@ -1,7 +1,17 @@
 import { UserWithoutPassword } from "@cleomacs/dbal/user"
+import { z } from "zod"
+import { success } from "./utils"
 
 // profile
 export const profileOutput = (user: UserWithoutPassword) => ({
   user,
 })
 export type ProfileOutput = ReturnType<typeof profileOutput>
+
+// Update user name
+export const updateUserNameInput = z.object({
+  name: z.string(),
+})
+export type UpdateUserNameInput = z.infer<typeof updateUserNameInput>
+export const updateUserNameOutput = () => success
+export type UpdateUserNameOutput = ReturnType<typeof updateUserNameOutput>
