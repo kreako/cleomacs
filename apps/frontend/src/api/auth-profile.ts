64aaac0dcb2b322d171c516ca55daed38f3fa6a0
kreako <olivier@kreako.fr>
2022-02-04 22:43:37 +0100

    Update the store automatically from profile query

--- apps/frontend/src/api/auth-profile.ts
+++ apps/frontend/src/api/auth-profile.ts
@@ -1,5 +1,6 @@
 import { ProfileOutput, UpdateUserNameInput } from "@cleomacs/api/auth-profile"
 import { useMutation, useQuery } from "react-query"
+import { useAuthStore } from "../stores/auth"
 import { keys } from "./query-key"
 import { get, put, retryQuery, UseMutationType } from "./utils"
 
@@ -7,9 +8,20 @@ const fetchProfile = async (): Promise<ProfileOutput> =>
   get("/auth-profile/profile")
 
 export const useProfile = () => {
+  const store = useAuthStore()
   return useQuery(keys.profile, fetchProfile, {
     useErrorBoundary: true,
     retry: retryQuery(["AuthenticationError"]),
+    onSuccess: ({ user }) => {
+      // update the store automatically after each profile query
+      store.update({
+        userId: user?.id,
+        membershipId: user?.lastMembershipId,
+        membershipRole: user?.lastMembership?.role,
+        organizationId: user?.lastMembership?.organizationId,
+        globalRole: user?.role,
+      })
+    },
   })
 }
 
