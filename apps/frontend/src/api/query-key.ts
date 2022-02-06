e4da7a92fff3b10b324842c5b701a4ce185940b8
kreako <olivier@kreako.fr>
2022-02-06 18:57:55 +0100

    Split query keys by scope

--- apps/frontend/src/api/query-key.ts
+++ apps/frontend/src/api/query-key.ts
@@ -1,5 +1,10 @@
-export const keys = {
-  profile: [{ scope: "profile" }] as const,
-  authPasswordTokenInfo: (token: string | null) =>
-    [{ scope: "auth-password", token }] as const,
+export const keysAuthProfile = {
+  profile: [{ scope: "profile", item: "profile" }] as const,
+  teams: [{ scope: "profile", item: "team" }] as const,
+  team: (organizationId: number | undefined) =>
+    [{ scope: "profile", item: "team", organizationId }] as const,
+}
+
+export const keysAuthPassword = {
+  tokenInfo: (token: string | null) => [{ scope: "auth-password", token }] as const,
 }
