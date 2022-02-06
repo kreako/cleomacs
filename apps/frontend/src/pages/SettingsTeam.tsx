49e73c13cb07f6e07a7202258fc9b472cc81efd7
kreako <olivier@kreako.fr>
2022-02-06 19:06:02 +0100

    Add a draft of SendInvitation in SettingsPage

--- apps/frontend/src/pages/SettingsTeam.tsx
+++ apps/frontend/src/pages/SettingsTeam.tsx
@@ -2,10 +2,17 @@ import type { TeamOutput } from "@cleomacs/api/auth-profile"
 import { useTeam } from "../api/auth-profile"
 import LoadingPage from "../components/LoadingPage"
 import RawError from "../components/RawError"
-import RoundPerson from "~icons/ic/round-person"
 import { ReactNode } from "react"
 import { formatDistance } from "date-fns"
 import { fr } from "date-fns/locale"
+import RoundPerson from "~icons/ic/round-person"
+import RoundPersonAdd from "~icons/ic/round-person-add"
+import { Disclosure } from "@headlessui/react"
+import { Field, Form } from "react-final-form"
+import { required, validateEmail } from "../utils/form"
+import LabelInput from "../components/LabelInput"
+import { NewInvitationInput } from "packages/api/auth-invitation"
+import { useNewInvitation } from "../api/auth-invitation"
 
 // Ability to unpack type[] to type
 type Unpacked<T> = T extends (infer U)[] ? U : T
@@ -40,11 +47,7 @@ function Pill({ children, color }: PillProps) {
     c = "bg-emerald-500"
   }
   return (
-    <div
-      className={`px-2 ${c} w-min rounded-full text-sm text-white font-bold`}
-    >
-      {children}
-    </div>
+    <div className={`px-2 ${c} w-min rounded-full text-sm font-bold text-white`}>{children}</div>
   )
 }
 
@@ -72,7 +75,7 @@ function MembershipUserName({ name, email }: MembershipUserNameProps) {
     return (
       <div className="flex items-baseline space-x-2">
         <div>{name}</div>
-        <div className="text-sky-700 text-sm">{email}</div>
+        <div className="text-sm text-sky-700">{email}</div>
       </div>
     )
   }
@@ -91,13 +94,9 @@ type MembershipUserProps = {
 
 function MembershipUser({ membership, user, now }: MembershipUserProps) {
   return (
-    <div className="flex space-x-4 items-center">
+    <div className="flex items-center space-x-4">
       <div className="w-[2em] flex-shrink-0 flex-grow-0">
-        <RoundPerson
-          width={"1.5em"}
-          height={"1.5em"}
-          className="text-sky-600"
-        />
+        <RoundPerson width={"1.5em"} height={"1.5em"} className="text-sky-600" />
       </div>
       <div className="w-5/12 flex-shrink-0">
         <MembershipUserName name={user.name} email={user.email} />
@@ -118,19 +117,11 @@ type MembershipInvitationProps = {
   now: Date
 }
 
-function MembershipInvitation({
-  membership,
-  invitation,
-  now,
-}: MembershipInvitationProps) {
+function MembershipInvitation({ membership, invitation, now }: MembershipInvitationProps) {
   return (
-    <div className="flex space-x-4 items-center">
+    <div className="flex items-center space-x-4">
       <div className="w-[2em] flex-shrink-0 flex-grow-0">
-        <RoundPerson
-          width={"1.5em"}
-          height={"1.5em"}
-          className="text-sky-600 opacity-25"
-        />
+        <RoundPerson width={"1.5em"} height={"1.5em"} className="text-sky-600 opacity-25" />
       </div>
       <div className="w-5/12 flex-shrink-0">
         <MembershipUserName name={invitation.name} email={invitation.email} />
@@ -151,24 +142,78 @@ type MembershipProps = {
 
 function Membership({ membership, now }: MembershipProps) {
   if (membership.user) {
-    return (
-      <MembershipUser
-        membership={membership}
-        user={membership.user}
-        now={now}
-      />
-    )
+    return <MembershipUser membership={membership} user={membership.user} now={now} />
   }
   if (membership.invitation) {
     return (
-      <MembershipInvitation
-        membership={membership}
-        invitation={membership.invitation}
-        now={now}
-      />
+      <MembershipInvitation membership={membership} invitation={membership.invitation} now={now} />
     )
   }
   return <div>What ? {JSON.stringify(membership)}</div>
+}
+
+function SendInvitation() {
+  const mutation = useNewInvitation({})
+  const onSubmit = (values: object) => {
+    const v = values as NewInvitationInput
+    mutation.mutate(v)
+  }
+  return (
+    <Disclosure>
+      <Disclosure.Button className=" flex items-center space-x-2 rounded-md border border-sky-600 py-1 px-2 shadow-md hover:bg-sky-100">
+        <div className="text-sky-900">Invitez un autre membre</div>
+        <RoundPersonAdd className="text-sky-600 opacity-75" width={"1em"} height={"1em"} />
+      </Disclosure.Button>
+      <Disclosure.Panel className="max-w-md">
+        <Form onSubmit={onSubmit}>
+          {({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <div className="mt-4">
+                <Field<string> name="name" validate={required("Le nom du futur membre")}>
+                  {({ input: { name, value, onChange }, meta: { error, touched } }) => (
+                    <LabelInput
+                      label="Le nom du futur membre"
+                      kind="text"
+                      name={name}
+                      value={value}
+                      onChange={onChange}
+                      error={error}
+                      touched={touched}
+                    />
+                  )}
+                </Field>
+              </div>
+              <div className="mt-4">
+                <Field<string> name="email" validate={validateEmail}>
+                  {({ input: { name, value, onChange }, meta: { error, touched } }) => (
+                    <LabelInput
+                      label="L'email du futur membre"
+                      kind="text"
+                      name={name}
+                      value={value}
+                      onChange={onChange}
+                      error={error}
+                      touched={touched}
+                    />
+                  )}
+                </Field>
+              </div>
+              <div className="mt-1 flex w-full justify-end">
+                <button
+                  type="submit"
+                  className="rounded-md bg-sky-600 py-1 px-2 text-sky-100 shadow-md"
+                >
+                  <div className="flex items-center justify-center space-x-2">
+                    <div>Envoyer</div>
+                  </div>
+                </button>
+              </div>
+            </form>
+          )}
+        </Form>
+      </Disclosure.Panel>
+    </Disclosure>
+  )
 }
 
 export default function SettingsTeam() {
@@ -179,28 +224,27 @@ export default function SettingsTeam() {
     return (
       <div className="pl-4">
         <div>
-          <div className="text-sky-900 font-bold text-sm">
-            L&apos;organisation
-          </div>
+          <div className="text-sm font-bold text-sky-900">L&apos;organisation</div>
           <div className="mt-2">{team.data.team.name}</div>
         </div>
         <div className="mt-12">
-          <div className="text-sky-900 font-bold text-sm">L&apos;équipe</div>
-          <div className="flex space-x-4 mt-4 mb-2 bg-sky-100 text-sky-700 opacity-70">
+          <div className="text-sm font-bold text-sky-900">L&apos;équipe</div>
+          <div className="mt-4 mb-2 flex space-x-4 bg-sky-100 text-sky-700 opacity-70">
             <div className="w-[2em] flex-shrink-0 flex-grow-0"></div>
-            <div className="w-5/12 flex-shrink-0 uppercase text-sm font-bold tracking-wide">
+            <div className="w-5/12 flex-shrink-0 text-sm font-bold uppercase tracking-wide">
               Nom
             </div>
             <div className="w-1/12 flex-shrink-0 flex-grow-0"></div>
-            <div className="uppercase text-sm font-bold tracking-wide">
-              Depuis
-            </div>
+            <div className="text-sm font-bold uppercase tracking-wide">Depuis</div>
           </div>
           <div>
             {team.data.team.memberships.map((m) => (
               <Membership membership={m} key={m.id} now={now} />
             ))}
           </div>
+        </div>
+        <div className="mt-4">
+          <SendInvitation />
         </div>
       </div>
     )
